95977597cea493b82a4c3c23401a8aa5e23a5012
wynne <664966415@qq.com>
2017-06-14 23:05:41 +0800

    itemDecoration

--- app/src/main/java/mvp/wyyne/douban/moviedouban/utils/TitleRecycleItemDecoration.java
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/utils/TitleRecycleItemDecoration.java
@@ -3,7 +3,10 @@ package mvp.wyyne.douban.moviedouban.utils;
 import android.animation.TimeInterpolator;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.util.TypedValue;
@@ -20,25 +23,87 @@ import mvp.wyyne.douban.moviedouban.api.bean.Subjects;
 public class TitleRecycleItemDecoration extends RecyclerView.ItemDecoration {
     private int mTitleHeight;
     private List<MovieType> mData;
+    private Paint mPaint;
+    private Rect mBound;
+    private static int COLOR_TITLE_BG = Color.parseColor("#FFDFDFDF");
+    private static int COLOR_TITLE_FONT = Color.parseColor("#FF000000");
+
 
     public TitleRecycleItemDecoration(Context context, List<MovieType> data) {
         super();
         mTitleHeight = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 30, context.getResources().getDisplayMetrics());
         mData = data;
+        mBound = new Rect();
+        mPaint = new Paint();
 
     }
 
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDraw(c, parent, state);
+        final int left = parent.getPaddingLeft();
+        final int right = parent.getWidth() - parent.getPaddingRight();
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams pams = (RecyclerView.LayoutParams) child.getLayoutParams();
+            int position = pams.getViewLayoutPosition();
+            if (position > -1) {
+                if (position == 0) {//等于0肯定要有title的
+                    drawTitleArea(c, left, right, child, pams, position);
+
+                } else {//其他的通过判断
+                    if (null != mData.get(position).getTags() && !mData.get(position).getTags().equals(mData.get(position - 1).getTags())) {
+                        //不为空 且跟前一个tag不一样了，说明是新的分类，也要title
+                        drawTitleArea(c, left, right, child, pams, position);
+                    } else {
+                        //none
+                    }
+                }
+            }
+        }
         Log.d("XXW", "onDraw");
     }
+
+
+    /**
+     * 绘制Title区域背景和文字的方法
+     *
+     * @param c
+     * @param left
+     * @param right
+     * @param child
+     * @param params
+     * @param position
+     */
+    private void drawTitleArea(Canvas c, int left, int right, View child, RecyclerView.LayoutParams params, int position) {//最先调用，绘制在最下层
+        mPaint.setColor(COLOR_TITLE_BG);
+        c.drawRect(left, child.getTop() - params.topMargin - mTitleHeight, right, child.getTop() - params.topMargin, mPaint);
+        mPaint.setColor(COLOR_TITLE_FONT);
+/*
+        Paint.FontMetricsInt fontMetrics = mPaint.getFontMetricsInt();
+        int baseline = (getMeasuredHeight() - fontMetrics.bottom + fontMetrics.top) / 2 - fontMetrics.top;*/
+
+        mPaint.getTextBounds(mData.get(position).getTags(), 0, mData.get(position).getTags().length(), mBound);
+        c.drawText(mData.get(position).getTags(), child.getPaddingLeft(), child.getTop() - params.topMargin - (mTitleHeight / 2 - mBound.height() / 2), mPaint);
+    }
+
 
 
     @Override
     public void onDrawOver(Canvas c, RecyclerView parent, RecyclerView.State state) {
         super.onDrawOver(c, parent, state);
-        Log.d("XXW", "onDrawOver");
+        int pos = ((LinearLayoutManager) parent.getLayoutManager()).findFirstVisibleItemPosition();
+        String tags = mData.get(pos).getTags();
+        View child = parent.findViewHolderForLayoutPosition(pos).itemView;
+        mPaint.setColor(COLOR_TITLE_BG);
+        c.drawRect(parent.getPaddingLeft(), parent.getPaddingTop(),
+                parent.getRight() - parent.getPaddingRight(), parent.getPaddingTop() + mTitleHeight, mPaint);
+        mPaint.setColor(COLOR_TITLE_FONT);
+        mPaint.setTextSize(28);
+        mPaint.getTextBounds(tags, 0, tags.length(), mBound);
+        c.drawText(tags, child.getPaddingLeft(), parent.getPaddingTop() + mTitleHeight - (mTitleHeight / 2 - mBound.height() / 2), mPaint);
+        Log.d("XXW", "onDrawOver-------" + mTitleHeight + "----Bound----" + mBound.height());
     }
 
     @Override
@@ -47,10 +112,10 @@ public class TitleRecycleItemDecoration extends RecyclerView.ItemDecoration {
         int position = ((RecyclerView.LayoutParams) view.getLayoutParams()).getViewLayoutPosition();
         if (position > -1) {
             if (position == 0) {
-                Log.d("XXW","position=0");
+                Log.d("XXW", "position=0");
                 outRect.set(0, mTitleHeight, 0, 0);
             } else {
-                Log.d("XXW","position>0");
+                Log.d("XXW", "position>0");
                 if (null != mData.get(position).getTags() && mData.get(position).getTags().equals(mData.get(position - 1).getTags())) {
                     outRect.set(0, mTitleHeight, 0, 0);
                 } else {

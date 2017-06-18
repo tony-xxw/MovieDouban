6be894fce5f979e611c634373e71bcaa021fad2f
wynne <664966415@qq.com>
2017-06-18 20:02:22 +0800

    detail

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/detail/DetailMovieActivity.java
@@ -0,0 +1,11 @@
+package mvp.wyyne.douban.moviedouban.detail;
+
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by XXW on 2017/6/18.
+ */
+
+public class DetailMovieActivity extends Fragment {
+}

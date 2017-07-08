5281a5313647ff94efc07985f42941df9b1d0442
wynne <664966415@qq.com>
2017-07-08 22:53:48 +0800

    head

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/adapter/PhotoFmAdapter.java
@@ -0,0 +1,43 @@
+package mvp.wyyne.douban.moviedouban.adapter;
+
+import android.content.Context;
+import android.util.Log;
+
+import java.util.List;
+
+import mvp.wyyne.douban.moviedouban.R;
+import mvp.wyyne.douban.moviedouban.api.bean.Photos;
+
+/**
+ * Created by XXW on 2017/7/8.
+ */
+
+public class PhotoFmAdapter extends BaseRvAdapter<Photos> {
+    public PhotoFmAdapter(Context context, List<Photos> data) {
+        super(context, data);
+    }
+
+    @Override
+    int getLayoutId() {
+        return R.layout.item_cast_article_fm;
+    }
+
+    @Override
+    void bindView(BaseItemViewHolder holder, int position) {
+        if (mList != null) {
+            Log.d("XXW", "photo--" + mList.get(position).getImage());
+            holder.setImgUrl(R.id.iv_cast, mList.get(position).getImage());
+        }
+
+    }
+
+    @Override
+    void bindHeadView(BaseItemViewHolder holder, int position) {
+
+    }
+
+    @Override
+    void bindFooterView(BaseItemViewHolder holder, int position) {
+
+    }
+}
--- app/src/main/java/mvp/wyyne/douban/moviedouban/detail/cast/CastArticleActivity.java
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/detail/cast/CastArticleActivity.java
@@ -1,20 +1,71 @@
 package mvp.wyyne.douban.moviedouban.detail.cast;
 
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.AppBarLayout;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.SimpleTarget;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import mvp.wyyne.douban.moviedouban.R;
+import mvp.wyyne.douban.moviedouban.adapter.PhotoFmAdapter;
 import mvp.wyyne.douban.moviedouban.api.bean.CastArticle;
+import mvp.wyyne.douban.moviedouban.api.bean.Photos;
 import mvp.wyyne.douban.moviedouban.home.BaseActivity;
 
 /**
  * Created by XXW on 2017/6/30.
  */
 
-public class CastArticleActivity extends BaseActivity<ICastPresent> implements ICastMain {
+public class CastArticleActivity extends BaseActivity<ICastPresent> implements ICastMain, AppBarLayout.OnOffsetChangedListener {
     //影人条目Id
     public static final String CAST_ID = "cast_id";
+    @BindView(R.id.iv_back)
+    ImageView mIvBack;
+    @BindView(R.id.tv_stills_title)
+    TextView mTvStillsTitle;
+    @BindView(R.id.iv_share)
+    ImageView mIvShare;
+    @BindView(R.id.tl_title)
+    Toolbar mTlTitle;
+    @BindView(R.id.iv_avatar)
+    ImageView mIvAvatar;
+    @BindView(R.id.rv_photo)
+    RecyclerView mRvPhoto;
+    @BindView(R.id.fl_avatars_bg)
+    FrameLayout mFlAvatarsBg;
+    @BindView(R.id.fl_content)
+    FrameLayout mFlContent;
+    @BindView(R.id.ll_title)
+    RelativeLayout mLlTitle;
     private String id;
+    private List<Photos> mPhotosList;
+    private PhotoFmAdapter mFmAdapter;
+    private Palette.Swatch swatch;
+    private Palette.Builder mPalette;
+    private Bitmap mDrawableBitmap;
+    private int boundHeight;
+    private CastArticle mCastArticle;
+
 
     @Override
     protected void refresh() {
@@ -32,7 +83,17 @@ public class CastArticleActivity extends BaseActivity<ICastPresent> implements I
             id = getIntent().getStringExtra(CAST_ID);
             Log.d("XXW", "id-->" + id);
         }
+        mPhotosList = new ArrayList<>();
         mPresent = new CastArticleImp(this);
+        setSupportActionBar(mTlTitle);
+
+        //设置影人相册Rv
+        mRvPhoto.setLayoutManager(new GridLayoutManager(this, 2));
+        mFmAdapter = new PhotoFmAdapter(this, mPhotosList);
+        mRvPhoto.setAdapter(mFmAdapter);
+
+
+        mIvShare.setVisibility(View.VISIBLE);
         mPresent.getCastArticle(id);
 
 
@@ -51,6 +112,71 @@ public class CastArticleActivity extends BaseActivity<ICastPresent> implements I
 
     @Override
     public void showPage(CastArticle article) {
+        mCastArticle = article;
+        mPhotosList = article.getPhotos();
+        Log.d("XXW", "--" + mPhotosList.size());
+        mFmAdapter.setList(mPhotosList);
+        mFmAdapter.notifyDataSetChanged();
+
+        setBackGroudBg(article.getAvatars().getLarge());
+    }
+
+    public void setBackGroudBg(String url) {
+        Glide.with(this).load(url).into(mIvAvatar);
+        Glide.with(this).load(url).asBitmap().into(new SimpleTarget<Bitmap>() {
+            @Override
+            public void onResourceReady(Bitmap resource, GlideAnimation<? super Bitmap> glideAnimation) {
+                if (resource != null) {
+                    mDrawableBitmap = resource;
+                    //设置颜色调试器
+                    mPalette = Palette.from(mDrawableBitmap);
+                    //颜色调试器回调监听
+                    mPalette.generate(new Palette.PaletteAsyncListener() {
+                        @Override
+                        public void onGenerated(Palette palette) {
+                            swatch = palette.getMutedSwatch();
+                            if (swatch != null) {
+                                mFlAvatarsBg.setBackgroundColor(swatch.getRgb());
+                                mLlTitle.setBackgroundColor(Color.TRANSPARENT);
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+
+    @Override
+    public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+        int y = Math.abs(verticalOffset);
+        if (mIvAvatar != null) {
+            boundHeight = mIvAvatar.getHeight();
+            if (y <= 0) {
+                titleHide();
+//                Log.d("XXW", "onOffsetChanged-----<0" + verticalOffset);
+                mLlTitle.setBackgroundColor(Color.argb((int) 0, 227, 29, 26));//AGB由相关工具获得，或者美工提供
+            } else if (y > 0 && y <= boundHeight) {
+//                Log.d("XXW", "onOffsetChanged----->0  <height"+"------"+verticalOffset);
+                mLlTitle.setBackgroundColor(ContextCompat.getColor(this, R.color.colorTranslucence));
+            } else {
+                titleShow();
+//                Log.d("XXW", "onOffsetChanged----->height---" + verticalOffset);
+                mLlTitle.setBackgroundColor(swatch.getRgb());
+            }
+        }
 
     }
+
+
+    public void titleHide() {
+        mTvStillsTitle.setTextColor(Color.WHITE);
+        mTvStillsTitle.setText(R.string.cast);
+    }
+
+    public void titleShow() {
+        mTvStillsTitle.setTextColor(Color.WHITE);
+        mTvStillsTitle.setText(mCastArticle.getName());
+    }
+
 }

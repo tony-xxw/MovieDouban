f98e9c91dcd33f6e77f4b46816bdfe640af92014
wynne <664966415@qq.com>
2018-05-09 17:48:01 +0800

    图片评论页面ItemUi 更新

--- app/src/main/java/mvp/wyyne/douban/moviedouban/detail/comment/photo/PhotoCommentAdapter.java
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/detail/comment/photo/PhotoCommentAdapter.java
@@ -1,6 +1,11 @@
 package mvp.wyyne.douban.moviedouban.detail.comment.photo;
 
 import android.content.Context;
+import android.text.TextUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import java.util.List;
 
@@ -29,7 +34,26 @@ public class PhotoCommentAdapter extends BaseRvAdapter<Reviews> {
 
     @Override
     public void bindView(BaseItemViewHolder holder, int position) {
-
+        ImageView avatar = holder.getView(R.id.riv_avatar);
+        TextView tvContent = holder.getView(R.id.tv_content);
+        TextView tvUserName = holder.getView(R.id.tv_user);
+        TextView tvCreateTime = holder.getView(R.id.tv_date);
+        String avatarUrl = mList.get(position).getAuthor().getAvatar();
+        String tvContentStr = mList.get(position).getContent();
+        String tvUserNameStr = mList.get(position).getAuthor().getName();
+        String tvCreateTimeStr = mList.get(position).getCreated_at();
+        if (!TextUtils.isEmpty(avatarUrl)) {
+            Glide.with(mContext).load(avatarUrl).into(avatar);
+        }
+        if (!TextUtils.isEmpty(tvContentStr)) {
+            tvContent.setText(tvContentStr);
+        }
+        if (!TextUtils.isEmpty(tvUserNameStr)) {
+            tvUserName.setText(tvUserNameStr);
+        }
+        if (!TextUtils.isEmpty(tvCreateTimeStr)) {
+            tvCreateTime.setText(tvCreateTimeStr);
+        }
     }
 
     @Override

bd0bb766e5f79b30ddaa395e4f29e71d0bbd3f5c
Wynne <a664966415@gmail.com>
2018-05-17 23:37:42 +0800

    妹子 查看Activity

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/oneself/photo/OneSelfPhotoActivity.java
@@ -0,0 +1,26 @@
+package mvp.wyyne.douban.moviedouban.oneself.photo;
+
+import mvp.wyyne.douban.moviedouban.R;
+import mvp.wyyne.douban.moviedouban.home.base.BaseActivity;
+
+/**
+ * @author Wynne
+ * @date 2018/5/17
+ */
+
+public class OneSelfPhotoActivity extends BaseActivity {
+    @Override
+    protected void refresh() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_oneself_photo;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

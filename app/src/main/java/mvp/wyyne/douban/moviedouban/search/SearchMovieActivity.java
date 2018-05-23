7822883d1836f392dfcc54c4ab2c41c953e5b2de
Wynne <a664966415@gmail.com>
2018-05-23 23:25:18 +0800

    搜索页面Activityy

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/search/SearchMovieActivity.java
@@ -0,0 +1,30 @@
+package mvp.wyyne.douban.moviedouban.search;
+
+
+import mvp.wyyne.douban.moviedouban.R;
+import mvp.wyyne.douban.moviedouban.home.base.BaseActivity;
+
+/**
+ * 搜索电影页面
+ *
+ * @author XXW
+ * @date 2017/6/3
+ */
+
+public class SearchMovieActivity extends BaseActivity {
+
+    @Override
+    protected void refresh() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_search_movie;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

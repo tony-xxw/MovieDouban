6a2af01ac90be1513065955c98db63686ba29c14
Wynne <a664966415@gmail.com>
2018-05-31 23:03:20 +0800

    empty

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/welfare/WelfarePhotoActivity.java
@@ -0,0 +1,25 @@
+package mvp.wyyne.douban.moviedouban.welfare;
+
+import mvp.wyyne.douban.moviedouban.home.base.BaseActivity;
+
+/**
+ * @author Wynne
+ * @date 2018/5/31
+ */
+
+public class WelfarePhotoActivity extends BaseActivity {
+    @Override
+    protected void refresh() {
+
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}

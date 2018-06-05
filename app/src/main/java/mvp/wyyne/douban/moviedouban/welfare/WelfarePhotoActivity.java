e3cfb1d4a7bd518290363d0e83b1aec4536a746b
wynne <664966415@qq.com>
2018-06-05 18:11:34 +0800

    dialogog

--- app/src/main/java/mvp/wyyne/douban/moviedouban/welfare/WelfarePhotoActivity.java
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/welfare/WelfarePhotoActivity.java
@@ -1,7 +1,11 @@
 package mvp.wyyne.douban.moviedouban.welfare;
 
+import android.app.AlertDialog;
 import android.graphics.Color;
 import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -18,11 +22,13 @@ import mvp.wyyne.douban.moviedouban.utils.StatusUtils;
  * @date 2018/5/31
  */
 
-public class WelfarePhotoActivity extends BaseActivity {
+public class WelfarePhotoActivity extends BaseActivity implements View.OnLongClickListener {
     public static final String TAG = WelfarePhotoActivity.class.getSimpleName();
     @BindView(R.id.iv_girl)
     ImageView ivGirl;
     private String mPhotoUrl;
+    private View dialogView;
+    private AlertDialog alertDialog;
 
     @Override
     protected void refresh() {
@@ -44,5 +50,27 @@ public class WelfarePhotoActivity extends BaseActivity {
         if (!TextUtils.isEmpty(mPhotoUrl)) {
             Glide.with(this).load(mPhotoUrl).into(ivGirl);
         }
+
+        ivGirl.setOnLongClickListener(this);
+        dialogView = LayoutInflater.from(this).inflate(R.layout.dialog_list_welfare_photo, null);
+
+        initDialog();
+    }
+
+    private void initDialog() {
+        alertDialog = new AlertDialog.Builder(this)
+                .setView(dialogView)
+                .create();
+        WindowManager windowManager = getWindowManager();
+        WindowManager.LayoutParams layoutParams = alertDialog.getWindow().getAttributes();
+        layoutParams.width = (int) (windowManager.getDefaultDisplay().getWidth() * 0.4);
+        alertDialog.getWindow().setAttributes(layoutParams);
+
+    }
+
+    @Override
+    public boolean onLongClick(View v) {
+        alertDialog.show();
+        return false;
     }
 }

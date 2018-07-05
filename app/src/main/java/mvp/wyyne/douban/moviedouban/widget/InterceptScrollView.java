59f3e82c90b67594c2f149901b6ae1268b9e08bb
Wynne <a664966415@gmail.com>
2018-07-05 23:58:21 +0800

    外部拦截的ScrollView

---
+++ app/src/main/java/mvp/wyyne/douban/moviedouban/widget/InterceptScrollView.java
@@ -0,0 +1,36 @@
+package mvp.wyyne.douban.moviedouban.widget;
+
+import android.content.Context;
+import android.support.v4.widget.NestedScrollView;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ *
+ * @author Wynne
+ * @date 2018/7/5
+ */
+
+public class InterceptScrollView extends NestedScrollView {
+    public InterceptScrollView(Context context) {
+        super(context);
+    }
+
+    public InterceptScrollView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public InterceptScrollView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        boolean intercept = false;
+        if (ev.getAction() == MotionEvent.ACTION_MOVE) {
+            intercept = true;
+        }
+        return intercept;
+    }
+}
